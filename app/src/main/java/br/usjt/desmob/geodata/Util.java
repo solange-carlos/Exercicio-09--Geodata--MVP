318878569579290e87891b4620f2198729c912bb
solange-carlos <solangeusjt2012@hotmail.com>
2017-12-10 21:46:04 -0200

    MVP
    Padronização MVP

---
+++ app/src/main/java/br/usjt/desmob/geodata/Util.java
@@ -0,0 +1,29 @@
+package br.usjt.desmob.geodata;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+
+/*
+ *  Criado por Solange Carlos em 10/12/2017.
+ *
+ */
+
+public class Util {
+    public static Drawable getDrawable(Context context, String nome){
+
+        Class<?> c = R.drawable.class;
+        try {
+            Field idField = c.getDeclaredField(nome);
+            int id = idField.getInt(idField);
+            return context.getResources().getDrawable(id, null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
